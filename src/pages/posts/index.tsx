ac0c5ec2b1ed7be19a6b925f0cf8d32221b9be2c
crlne <carol.lages@outlook.com>
2021-03-27 14:48:09 -0300

    menu navigation component

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import Head from 'next/head';
-import React from 'react';
 import styles from './styles.module.scss';
 import { getPrismicClient } from '../../services/prismic'
 import Prismic from '@prismicio/client'
 import { GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom'
 
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
           <Head>
@@ -15,21 +27,13 @@ export default function Posts() {
 
           <main className={styles.container}>
               <div className={styles.posts}>
-                  <a href="#">
-                      <time>26 de Março de 2021</time>
-                      <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                      <p>In this guide, you will learn how to crate a Monorepo to manage multiple packages with a</p>
-                  </a>
-                  <a href="#">
-                      <time>26 de Março de 2021</time>
-                      <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                      <p>In this guide, you will learn how to crate a Monorepo to manage multiple packages with a</p>
-                  </a>
-                  <a href="#">
-                      <time>26 de Março de 2021</time>
-                      <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                      <p>In this guide, you will learn how to crate a Monorepo to manage multiple packages with a</p>
-                  </a>
+                  { posts.map(post => (
+                    <a key={post.slug} href="#">
+                    <time>{post.updatedAt}</time>
+                    <strong>{post.title}</strong>
+                    <p>{post.excerpt}</p>
+                    </a>
+                  ))}
               </div>
           </main>
         </>
@@ -47,10 +51,26 @@ export const getStaticProps: GetStaticProps = async () => {
         pageSize: 100,
     })
 
-    console.log(response)
+
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'numeric',
+                year: 'numeric'
+            })
+        };
+    });
+
 
     return {
-        props: {}
+        props: {
+            posts
+        }
     }
 
 }
